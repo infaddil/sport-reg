382a62d84ab75589db09dbb151f1553abe4f1f80
infaddil <mohdfaddilintan@gmail.com>
2024-04-23 16:06:53 +0800

    Replacing the credentials

--- connection.php
+++ connection.php
@@ -6,11 +6,11 @@ $dbUrl = "mysql://ddjvbu2ey3xesqfq:c7wgxv73ff04gouk@ipobfcpvprjpmdo9.cbetxkdyhws
 $components = parse_url($dbUrl);
 
 // Extract the hostname, username, password, and database name from the URL
-$namahost = $components['host'];
-$username_SQL = $components['user'];
-$katalaluan_SQL = $components['pass'];
-$port = $components['port'];
-$nama_pangkalan_data = substr($components['path'], 1); // Remove the leading slash from the path to get the database name
+$namahost = $components['ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
+$username_SQL = $components['ddjvbu2ey3xesqfq'];
+$katalaluan_SQL = $components['c7wgxv73ff04gouk'];
+$port = $components['3306'];
+$nama_pangkalan_data = substr($components['fh3418remojvoxud'], 1); // Remove the leading slash from the path to get the database name
 
 // Create a connection to the MySQL database
 $condb = mysqli_connect($namahost, $username_SQL, $katalaluan_SQL, $nama_pangkalan_data, $port);
